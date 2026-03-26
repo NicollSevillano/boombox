44c827b875da398c1b42145c33c0869a3f2c03a8
Nicoll Sevillano <112912197+NicollSevillano@users.noreply.github.com>
2026-03-25 22:56:36 -0300

    Update ffmpeg executable path in radiobot.py

--- radiobot.py
+++ radiobot.py
@@ -79,7 +79,7 @@ async def reconectar(voice_client, url):
         try:
             source = await FFmpegOpusAudio.from_probe(
                 url,
-                executable="ffmpeg",
+                executable="/usr/bin/ffmpeg",
                 before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
                 options="-vn"
             )
