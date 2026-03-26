3d75972287c834f7a5c2c8e7775953ba5f6ec9c1
Nicoll Sevillano <112912197+NicollSevillano@users.noreply.github.com>
2026-03-26 19:19:40 -0300

    Change FFmpeg executable path to 'ffmpeg'

--- radiobot.py
+++ radiobot.py
@@ -79,7 +79,7 @@ async def reconectar(voice_client, url):
         try:
             source = await FFmpegOpusAudio.from_probe(
                 url,
-                executable="/usr/bin/ffmpeg",
+                executable="ffmpeg",
                 before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
                 options="-vn"
             )
@@ -120,7 +120,7 @@ async def reproducir(interaction_or_ctx, url: str, es_interaction: bool = True):
         # USANDO EL MÉTODO ASÍNCRONO from_probe
         source = await FFmpegOpusAudio.from_probe(
             url,
-            executable="/usr/bin/ffmpeg",
+            executable="ffmpeg",
             before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
             options="-vn"
         )
